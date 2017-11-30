15fe4caa625666fdd7ff4e9088938d96260a9267
AXAz0r <thealeksaradovic@gmail.com>
2017-11-30 16:44:01 +0100

    Fix Variable Quotation

--- linux/ubuntu/Xenial.sh
+++ linux/ubuntu/Xenial.sh
@@ -40,7 +40,7 @@ system_deps=(
     libreadline-gplv2-dev libncursesw5-dev libssl-dev
     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 )
-(sudo apt-get update -qq && sudo apt-get -qq install -y "${system_deps[*]}") \
+(sudo apt-get update -qq && sudo apt-get -qq install -y ${system_deps[*]}) \
 || abort "Failed to install system dependencies"
 
 echo "Installing Python 3.6.3..."
