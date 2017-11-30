ac0f710dd88ca5f1810cc0ee32f52ad16540d7ed
AXAz0r <thealeksaradovic@gmail.com>
2017-11-30 16:58:17 +0100

    Add Missing Systemd Dependencies

--- linux/ubuntu/Xenial.sh
+++ linux/ubuntu/Xenial.sh
@@ -36,7 +36,8 @@ echo "Starting Apex Sigma installation."
 
 echo "Installing required APT packages..."
 system_deps=(
-    git ffmpeg build-essential checkinstall
+    git ffmpeg build-essential libsystemd-journal-dev
+    checkinstall libsystemd-daemon-dev libsystemd-dev
     libreadline-gplv2-dev libncursesw5-dev libssl-dev
     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 )
